f82ae03d6b605f5da1627c9d92c98a11a87320e5
Radek Szymczyszyn <lavrin@gmail.com>
2016-03-10 23:13:38 +0100

    Fill in some code in docsh-example module

--- src/docsh-example.erl
+++ src/docsh-example.erl
@@ -1,13 +1,29 @@
 -module('docsh-example').
 
 %% API exports
--export([]).
+-export([hello/1]).
+
+-export_type([name/0]).
+
+-type name() :: string().
 
 %%====================================================================
 %% API functions
 %%====================================================================
 
+%% @doc Greet the person whose Name is passed in.
+-spec hello(name()) -> ok.
+hello(Name) ->
+    print("Hello, ~s!\n", [Name]).
 
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+-spec print(io:format(), [term()]) -> ok.
+print(Fmt, Args) ->
+    print(standard_io, Fmt, Args).
+
+-spec print(io:device(), io:format(), [term()]) -> ok.
+print(Handle, Fmt, Args) ->
+    io:format(Handle, Fmt, Args).
